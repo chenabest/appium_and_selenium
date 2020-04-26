8ab8d297cda61d540193c7e802adfb1bb933e92a
陈爱珍 <57119799+chenabest@users.noreply.github.com>
2020-04-27 03:18:34 +0800

    Update appium_utils.py

--- tools/appium_utils.py
+++ tools/appium_utils.py
@@ -16,45 +16,8 @@ import socket
 from appium_robot.robot.wechat_work.setting import XPATH_OBJECT_ALL_VERSIONS
 
 
-def start_server(serial: str, port: int, executable=True) -> None:
-    """
-    :param port:
-    :param serial:
-    :return:
-    """
-    chromedriver_path = get_chromedriver_path()
-    print(chromedriver_path)
-    if executable:
-        cmd = ('echo $SHELL; appium --chromedriver-executable ' + chromedriver_path +
-               ' --address 127.0.0.1' + ' --port ' + str(port) +
-               ' --bootstrap-port ' + str(port - 2000) + ' --default-capabilities ' + """'{"udid":"%s"}'""" % serial +
-               ' --log-level error &')
-    else:
-        cmd = ('echo $SHELL; appium' +
-               ' --address 127.0.0.1' + ' --port ' + str(port) +
-               ' --bootstrap-port ' + str(port - 2000) + ' --default-capabilities ' + """'{"udid":"%s"}'""" % serial +
-               ' --log-level error &')
-    subprocess.Popen(cmd, shell=True, executable='/bin/bash')
-    # os.popen(cmd)
-
-
-def kill_server(serial, description):
-    """
-    kill adb
-    :return: None
-    """
-    list_adb = "ps aux | grep %s | grep -v %s | grep -v tmux" % (serial, description)
-    find_process = os.popen(list_adb).readlines()
-    for process in find_process:
-        if "grep" not in process:
-            print("kill ", process)
-            pid = re.search(r'\w+\s+(\d+)', process).group(1)
-            kill_command = "kill -9 %s" % pid
-            os.system(kill_command)
-    print("kill %s adb server" % serial)
-
-
 def init_driver(serial=None, appPackage="com.tencent.mm", appActivity=".ui.LauncherUI"):
+    """功能：生成driver对象"""
     flag = socket.gethostname() == 'achens-iMac.lan'
     serial = get_valid_serial(serial)
     port = serial_port.get(serial, None)
@@ -64,12 +27,13 @@ def init_driver(serial=None, appPackage="com.tencent.mm", appActivity=".ui.Launc
             port = get_valid_port()
             print('获取到的端口号为：%s' % port)
         else:
-            raise RuntimeError(f'文件{PROJECT_PATH}/appium_robot/robot/__init__.py中没有配置该手机的端口号！')
+            raise RuntimeError(f'{serial} 文件{PROJECT_PATH}/appium_robot/robot/__init__.py中没有配置该手机的端口号！')
     print(serial, port)
     desired_caps = {
         "platformName": "Android",
         "deviceName": serial,
         "udid": serial,
+        "systemPort": port + 4000,
         "appPackage": appPackage,
         "appActivity": appActivity,
         "noReset": "True",
@@ -95,6 +59,10 @@ def init_driver(serial=None, appPackage="com.tencent.mm", appActivity=".ui.Launc
                 print('新端口号：%s' % port)
             print(serial, port)
             time.sleep(6)
+
+
+def lock_screen(serial):
+    print(os.popen(f'adb -s {serial} shell input keyevent 26').read())
 
 
 def air_plane(state, serial):
@@ -156,6 +124,14 @@ def get_current_activity(driver, poll_frequency=2, duration=6, max_time=8):
 
 
 def wechat_login(driver, account, password, wait_time=20):
+    """
+    微信自动登录
+    :param driver: MyDriver对象
+    :param account: 微信号
+    :param password: 密码
+    :param wait_time: 
+    :return: 
+    """
     while "正在载入数据" in driver.page_source:
         time.sleep(30)
     while '允许' in driver.page_source:
@@ -200,7 +176,25 @@ def wechat_login(driver, account, password, wait_time=20):
             return True
 
 
+def wechat_switch(serial, user=999):
+    """
+    功能：在主微信与分身微信之间切换
+    :param serial: 序列号
+    :param user: 999表示分身, 0表示原用户
+    :return: 
+    """
+    cmd = "adb -s {} shell am start --user {} " \
+          "-n com.tencent.mm/.ui.LauncherUI".format(serial, user)
+    os.system(cmd)
+    time.sleep(10)
+
+
 def wechat_close_new_message_notification(driver):
+    """
+    功能：关闭微信新消息提醒
+    :param driver: MyDriver对象
+    :return: 
+    """
     print("关闭新消息提醒...")
     driver.ai_click(
         '//android.widget.TextView[@text="我"]',
@@ -230,13 +224,20 @@ def wechat_close_new_message_notification(driver):
 
 
 def clear_memory(driver, messenger=None):
+    """
+    当手机存储空间不足时清理内存
+    :param driver: MyDriver或者WebDriver对象
+    :param messenger: 信使，发送微信消息通知
+    :return: 
+    """
     try:
         source = get_command()
         file_name = get_run_file_name()
         page_source = driver.page_source
-        if re.search('清理|内存|空间|不足|满', page_source):
+        match = re.search('清理|内存|存储空间|不足', page_source)
+        if match:
             if messenger:
-                messenger.send_message(f'{file_name}：内存空间不足',
+                messenger.send_message(f'{file_name}：内存空间不足, 匹配关键字【{match.group(0)}】',
                                        source=source)
                 time.sleep(30)
             print('清内存...')
@@ -249,29 +250,86 @@ def clear_memory(driver, messenger=None):
         print(e.__repr__())
 
 
+def get_all_package_names(serial):
+    lines = os.popen(f'adb -s {serial} shell pm list packages').readlines()
+    packages = set()
+    for line in lines:
+        packages.add(line.strip().split(':')[-1])
+    return packages
+
+
+def is_package_installed(package_name, serial=None):
+    serial = serial or get_all_online_serials().pop()
+    packages = get_all_package_names(serial)
+    return package_name in packages
+
+
+def get_manufacturer_name(serial):
+    """获取手机厂商"""
+    manufacturer_name = subprocess.check_output(f'adb -s {serial} -d shell getprop ro.product.brand'.split()).\
+        decode('utf-8').strip()
+    return manufacturer_name
+
+
+def get_platform_version(serial):
+    platform_version = subprocess.check_output(f'adb -s {serial} -d shell getprop ro.build.version.release'.split()).\
+        decode('utf-8').strip()
+    return platform_version
+
+
+def get_free_storage_size(serial, manufacturer=None):
+    if manufacturer is None:
+        manufacturer = get_manufacturer_name(serial)
+    if manufacturer == 'meizu':
+        result = os.popen("""adb -s %s shell df /sdcard | grep /sdcard | awk '{print $4}'""" % serial).read().strip()
+    else:
+        result = os.popen("adb -s %s shell df /sdcard -h | grep storage | awk '{print $4}'" % serial).read().strip()
+    if not re.match('\d', result):
+        print(f'获取{serial}剩余存储空间大小失败！')
+        return ''
+    return result
+
+
 def get_wechat_version(serial):
+    """获取微信版本号"""
     line = os.popen(f'adb -s {serial} shell dumpsys package com.tencent.mm | grep versionName').read().strip()
     print(line)
     match = re.search('versionName=(\S+)', line)
     if match:
         return match.group(1)
     else:
-        raise RuntimeError('获取微信版本号失败，请检查是否安装了微信')
+        raise RuntimeError('获取微信版本号失败，请检查设备是否正常连接到电脑，是否安装了微信')
+
+
+def get_package_version(package_name, serial):
+    line = os.popen(f'adb -s {serial} shell dumpsys package {package_name} | grep versionName').read().strip()
+    print(line)
+    match = re.search('versionName=(\S+)', line)
+    if match:
+        return match.group(1)
+    else:
+        raise RuntimeError(f'获取{package_name}版本号失败，请检查设备是否正常连接到电脑，是否安装了{package_name}')
 
 
 def get_all_online_serials():
+    """获取所有连接到电脑的手机序列号"""
     lines = os.popen('adb devices | grep -v attached | grep device ').readlines()
     if lines:
-        serials = []
+        serials = set()
         for line in lines:
-            serials.append(line.split('\t')[0])
+            serials.add(line.split('\t')[0])
         return serials
 
 
 serial_imei_dict = {}
 
 
-def get_imei2(serial):
+def get_imei_meizu_by_dialer(serial):
+    """
+    魅族手机：通过模拟拨号获取imei
+    :param serial: 手机序列号
+    :return: imei
+    """
     global serial_imei_dict
     driver = init_driver(serial=serial, appPackage='com.android.dialer', appActivity='.DialtactsActivity')
     if '确定' in driver.page_source:
@@ -291,7 +349,13 @@ def get_imei2(serial):
         print(f'{serial}: 获取imei失败')
 
 
-def get_imei(serial, max_tries=3):
+def get_imei_meizu_from_cellphone_info(serial, max_tries=3):
+    """
+    魅族手机：通过模拟点击，设置-》关于手机, 页面有imei信息
+    :param serial: 手机序列号
+    :param max_tries: 
+    :return: 
+    """
     global serial_imei_dict
     for times in range(max_tries):
         driver = init_driver(serial=serial, appPackage='com.android.settings', appActivity='.Settings')
@@ -306,7 +370,21 @@ def get_imei(serial, max_tries=3):
     print(f'{serial}: 获取imei失败')
 
 
-def get_all_imeis(serials=None, func=get_imei):
+def get_imei_oneplus_by_dialer(serial):
+    pass
+
+
+def get_imei_by_adb_call(serial):
+    """魅族、一加通用, 但是个别获取的是IMEI2的值，个别会获取不到"""
+    result = os.popen(f'adb -s {serial} shell service call iphonesubinfo 1').read()
+    numbers = re.findall('(\d)\.', result)
+    if numbers:
+        imei = ''.join(numbers)
+        serial_imei_dict[serial] = imei
+        return imei
+
+
+def get_all_imeis(serials=None, func=get_imei_by_adb_call):
     serials = serials or get_all_online_serials()
     global serial_imei_dict
     print(serials)
@@ -322,6 +400,7 @@ def get_all_imeis(serials=None, func=get_imei):
 
 
 def wechat_get_id_and_nickname(serial, driver=None, raise_exception=False):
+    """获取微信号和昵称"""
     MyDriver = __import__('appium_robot.robot.tools.my_driver_and_element', fromlist=['MyDriver']).MyDriver
     driver = driver or init_driver(serial, appPackage="com.tencent.mm", appActivity=".ui.LauncherUI")
     my_driver = MyDriver(driver, serial=serial, position_dict={})
@@ -332,7 +411,7 @@ def wechat_get_id_and_nickname(serial, driver=None, raise_exception=False):
             xpath_object.me_text, expect_element=xpath_object.get_wechat_id,
             raise_exception=False, max_retries=1)
         if not wechat_id_element:
-            input('1'*100)
+            print('1'*100)
             wechat_id_element = my_driver.adb_tap(
                 position=(630, 1230), expect_element=xpath_object.get_wechat_id)
         text = wechat_id_element.text
@@ -341,7 +420,6 @@ def wechat_get_id_and_nickname(serial, driver=None, raise_exception=False):
         nickname_element = my_driver.find_element_by_xpath(xpath_object.get_nickname)
         nickname = nickname_element.text
         print(f'昵称：{nickname}')
-        my_driver.quit()
         return wechat_id, nickname
     except Exception as e:
         print(f'{serial}: 获取微信号和昵称失败！')
@@ -352,7 +430,7 @@ def wechat_get_id_and_nickname(serial, driver=None, raise_exception=False):
 
 
 def debug_wechat(serial):
-    # 打开微信，进入ipdb调试模式
+    """功能：打开微信，进入ipdb调试模式"""
     from appium_robot.robot.tools.my_driver_and_element import MyDriver, MyElement
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
@@ -377,5 +455,5 @@ if __name__ == '__main__':
     python3 -m appium_robot.robot.tools.appium_functools --serial 710MVBR723R3Y
     """
     show_module(form='*')
-    serial = get_valid_serial(sys.argv[2])
-    debug_wechat(serial)
+    _serial = get_valid_serial(sys.argv[2])
+    debug_wechat(_serial)
